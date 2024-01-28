92a1f2b29d925c9368052d6195682ef019532342
Ted Anderson <111665720+tedanderson2299@users.noreply.github.com>
2024-01-27 22:30:49 -0500

    update

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import initScrollReveal from "./scripts/scrollReveal";
+import initScrollReveal from "/scripts/scrollReveal";
 import initTiltEffect from "./scripts/tiltAnimation";
 import { targetElements, defaultProps } from "./data/scrollRevealConfig";
 
